29b05e30b8c57a36a540ed681681b1fe7fc4bc33
nrcool <nrcoolove@yahoo.com>
2021-05-06 10:12:24 +0200

    initial commit

---
+++ precache-manifest.7de0ecb04945781e377150a09b4d6e37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375e19717b4e5a5878677be4790d5f90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a36c00f77374d4b74835",
+    "url": "./static/css/main.eb490982.chunk.css"
+  },
+  {
+    "revision": "cf96070990999b9e4a85",
+    "url": "./static/js/2.6bdcfe48.chunk.js"
+  },
+  {
+    "revision": "a36c00f77374d4b74835",
+    "url": "./static/js/main.cc659d71.chunk.js"
+  },
+  {
+    "revision": "baab4f6995ba25947e6f",
+    "url": "./static/js/runtime-main.f5f70450.js"
+  }
+]);
